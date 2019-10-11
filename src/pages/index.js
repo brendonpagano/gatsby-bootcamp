e21558036c1775df241aac3cc432e67771cf18e2
annatsu <brendonpagano@gmail.com>
2019-10-10 23:45:08 -0300

    Play around with `useStaticQuery` and Gatsby's graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+import Header from "../components/Header"
+
+const IndexPage = () => (
+  <div>
+    <Header />
+    <p>My name is Brendon</p>
+    <ul>
+      <li>
+        <Link to="/blog">Blog</Link>
+      </li>
+    </ul>
+  </div>
+)
+
+export default IndexPage
